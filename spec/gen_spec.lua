bda10e345e13ec179dc6b5344d02591b2f973830
Corey Williamson <euclidianAce@protonmail.com>
2020-09-20 02:26:28 -0500

    spec: fill gen_spec pendings

--- spec/gen_spec.lua
+++ spec/gen_spec.lua
@@ -2,15 +2,66 @@
 local util = require("spec.util")
 
 describe("gen command", function()
+   local proj = util.run_mock_project
    util.do_setup(setup, teardown)
-   pending("should generate a properly named file", function()
+   it("should generate a properly named file", function()
+      proj(finally, {
+         dir = {"my_file.tl"},
+         generated = {"my_file.lua"},
+         command = "gen",
+         args = {"my_file.tl"},
+      })
    end)
-   pending("should generate many files when provided with many arguments", function()
+   it("should generate many files when provided with many arguments", function()
+      proj(finally, {
+         dir = {
+            "my_file.tl",
+            "my_other_file.tl",
+            "foo.tl",
+            "yet_another_file.tl",
+            "bar.tl",
+         },
+         generated = {
+            "my_file.lua",
+            "my_other_file.lua",
+            "foo.lua",
+            "yet_another_file.lua",
+            "bar.lua",
+         },
+         command = "gen",
+         args = {
+            "my_file.tl",
+            "my_other_file.tl",
+            "foo.tl",
+            "yet_another_file.tl",
+            "bar.tl",
+         },
+      })
    end)
    describe("-o --output", function()
-      pending("should generate a properly named file", function()
+      it("should generate a properly named file", function()
+         proj(finally, {
+            dir = {"my_file.tl"},
+            generated = {"a_cool_name_for_a_file.lua"},
+            command = "gen",
+            args = {"my_file.tl --output a_cool_name_for_a_file.lua"},
+         })
       end)
-      pending("should not work when given multiple files", function()
+      it("should not work when given multiple files", function()
+         proj(finally, {
+            dir = {
+               "my_file.tl",
+               "my_other_file.tl"
+            },
+            generated = {},
+            command = "gen",
+            args = {
+               "my_file.tl",
+               "my_other_file.tl",
+               "--output a_cool_name_for_a_file.lua"
+            },
+            pipe_result = { nil, "exit", 1 },
+         })
       end)
    end)
 end)
