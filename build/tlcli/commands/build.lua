8b92709d51313170e03ef05277778c01954b67ce
Corey Williamson <euclidianAce@protonmail.com>
2020-10-02 18:16:43 -0500

    build: initial work for multiple C modules

--- build/tlcli/commands/build.lua
+++ build/tlcli/commands/build.lua
@@ -22,12 +22,29 @@ local options = util.protected_proxy({
    source_dir = ".",
    build_dir = ".",
    object_dir = ".",
-   module_name = "",
 })
 
 local flags = util.protected_proxy({
    keep_going = false,
 })
+
+local CModule = {}
+
+
+
+
+
+
+local c_modules = {}
+
+local function should_recompile_c_module(mod)
+   for k, v in pairs(mod.source) do
+      if v then
+         return true
+      end
+   end
+   return false
+end
 
 local build = {
    name = "build",
@@ -69,6 +86,7 @@ local build = {
       end
 
 
+
       local parents_that_exist = {}
       local function check_parents(dirname)
          for parent in fs.path_parents(dirname) do
@@ -94,6 +112,7 @@ local build = {
          return true
       end
 
+
       if util.error_if(
 fs.is_absolute(src_dir),
 "Source directory (" .. ansi.bright.yellow(src_dir) .. ") is not relative") then
@@ -115,6 +134,7 @@ fs.is_absolute(obj_dir),
          return 1
       end
 
+
       local p = io.popen("stty size")
       local columns = math.floor(tonumber(p:read("*a"):match("%d+ (%d+)")) / 2.5)
       p:close()
@@ -132,6 +152,7 @@ fs.is_absolute(obj_dir),
          io.stdout:write(step, ": ", fname, "\n")
          b:draw(io.stdout)
       end
+
 
       local function is_source_newer(source_path, target_path)
          local retval = false
@@ -156,8 +177,6 @@ fs.is_absolute(obj_dir),
       local function get_o(input)
          return input:sub(1, -2) .. "o"
       end
-
-      local o_files = {}
 
       local update_so = false
       local exit = 0
@@ -220,47 +239,87 @@ options.exclude) do
                total_steps = total_steps + 2
             end
 
-         elseif fs.get_extension(input_file) == "c" then
-            if options.module_name == "" then
-               log.error("Build error: c files are present in project, but a module name wasn't specified")
-               return 1
+         end
+      end
+
+      for modname, moddata in pairs(c_modules) do
+
+
+
+         for input_file in fs.match(
+src_dir,
+moddata.include or {},
+moddata.exclude or {}) do
+
+            if fs.get_extension(input_file) == "c" then
+
+
+               local output_file = get_o(input_file)
+
+               if src_dir ~= "." then
+                  output_file = output_file:sub(#src_dir + 2, -1)
+               end
+               if obj_dir ~= "." then
+                  output_file = fs.path_concat(obj_dir, output_file)
+               end
+
+               moddata.source[input_file] = false
+               if is_source_newer(input_file, output_file) then
+                  moddata.source[input_file] = true
+                  check_parents(output_file)
+                  scheduler.schedule_wrap(function()
+
+                     coroutine.yield()
+                     coroutine.yield()
+                     draw_progress("Compiling", input_file)
+                     local cmd = "cc -c " .. input_file .. " -o " .. output_file .. " 2>&1"
+                     log.verbose("%s", ansi.bright.magenta(cmd))
+                     local p = io.popen(cmd)
+                     local output = p:read("*a")
+                     local exit_ok = p:close()
+                     if not exit_ok then
+                        log.error("Error compiling %s:\nCommand run: %s\n\n%s", ansi.bright.yellow(input_file), cmd, output)
+                        exit = 1
+                        fatal_err = output
+                     else
+                        log.normal("Compiled %s -> %s", ansi.bright.yellow(input_file), ansi.bright.green(output_file))
+                        b:step()
+                     end
+                  end)
+                  total_steps = total_steps + 1
+               end
+               table.insert(moddata.o_files, output_file)
+
             end
+         end
+      end
 
-            local output_file = get_o(input_file)
-
-            assert(check_parents(output_file))
-
-            if src_dir ~= "." then
-               output_file = output_file:sub(#src_dir + 2, -1)
-            end
-            if obj_dir ~= "." then
-               output_file = fs.path_concat(obj_dir, output_file)
-            end
-
-            if is_source_newer(input_file, output_file) then
+      local c_mod_scheduler = task.scheduler("round-robin")
+      for name, mod in pairs(c_modules) do
+         if should_recompile_c_module(mod) then
+            total_steps = total_steps + 1
+            c_mod_scheduler.schedule_wrap(function()
+               local output_file = name .. "." .. fs.get_shared_library_ext()
+               if src_dir ~= "." then
+                  output_file = output_file:sub(#src_dir + 2, -1)
+               end
+               if build_dir ~= "." then
+                  output_file = fs.path_concat(build_dir, output_file)
+               end
                check_parents(output_file)
-               update_so = true
-               scheduler.schedule_wrap(function()
-
-                  coroutine.yield()
-                  coroutine.yield()
-                  draw_progress("Compiling", input_file)
-                  local cmd = "cc -c " .. input_file .. " -o " .. output_file .. " 2>&1"
-                  local p = io.popen(cmd)
-                  local output = p:read("*a")
-                  local exit_ok = p:close()
-                  if not exit_ok then
-                     log.error("Error compiling %s:\nCommand run: %s\n\n%s", ansi.bright.yellow(input_file), cmd, output)
-                     exit = 1
-                     fatal_err = output
-                  else
-                     log.normal("Compiled %s -> %s", ansi.bright.yellow(input_file), ansi.bright.green(output_file))
-                     b:step()
-                  end
-               end)
-               total_steps = total_steps + 1
-            end
-            table.insert(o_files, output_file)
+               draw_progress("Compiling", ansi.bright.yellow(output_file))
+               local cmd = "cc -fPIC -shared -o " .. output_file .. " " .. table.concat(mod.o_files, " ") .. " 2>&1"
+               log.verbose("%s", ansi.bright.magenta(cmd))
+               local p = io.popen(cmd)
+               local output = p:read("*a")
+               local exit_ok = p:close()
+               if not exit_ok then
+                  log.error("Error compiling module %s:\nCommand run: %s\n\n%s", ansi.bright.green(output_file), cmd, output)
+                  exit = 1
+               else
+                  log.normal("Compiled Module %s", ansi.bright.green(output_file))
+               end
+            end)
          end
       end
 
@@ -272,29 +331,9 @@ options.exclude) do
       b:set_total_steps(total_steps)
 
       io.write("\n")
-      scheduler.run()
 
-      if not fatal_err and (update_so or args.update_all) and #o_files > 0 then
-         local output_file = options.module_name
-         if src_dir ~= "." then
-            output_file = output_file:sub(#src_dir + 2, -1)
-         end
-         if build_dir ~= "." then
-            output_file = fs.path_concat(build_dir, output_file)
-         end
-         check_parents(output_file)
-         draw_progress("Compiling", ansi.bright.yellow(output_file))
-         local cmd = "cc -fPIC -shared -o " .. output_file .. " " .. table.concat(o_files, " ")
-         local p = io.popen(cmd)
-         local output = p:read("*a")
-         local exit_ok = p:close()
-         if not exit_ok then
-            log.error("Error compiling module %s:\nCommand run: %s\n\n%s", ansi.bright.green(output_file), cmd, output)
-            exit = 1
-         else
-            log.normal("Compiled Module %s", ansi.bright.green(output_file))
-         end
-      end
+      scheduler.run()
+      c_mod_scheduler.run()
 
       ansi.cursor.up(1)
       ansi.cursor.set_column(0)
@@ -323,8 +362,58 @@ options.exclude) do
                options[k] = v
             end
          end
+      elseif opt == "cmodule" then
+         return function(t)
+
+            if not t.name or type(t.name) ~= "string" then
+               log.error("Name not provided for c module")
+               return
+            end
+            if c_modules[t.name] then
+               log.error("Module \"%s\" was already specified")
+               return
+            end
+            if not (t.include or t.exclude) then
+               log.error("Specify source files for c module \"%s\" with the \"include\" and \"exclude\" fields", t.name)
+               return
+            end
+            local m = {
+               include = {},
+               exclude = {},
+               source = {},
+               o_files = {},
+            }
+            if t.include then
+               for i, v in ipairs(t.include) do
+                  if type(v) == "string" then
+                     table.insert(m.include, v)
+                  else
+                     log.warn("Non string entry in \"include\" field")
+                  end
+               end
+            end
+            if t.exclude then
+               for i, v in ipairs(t.exclude) do
+                  if type(v) == "string" then
+                     table.insert(m.exclude, v)
+                  else
+                     log.warn("Non string entry in \"exclude\" field")
+                  end
+               end
+            end
+            c_modules[t.name] = m
+         end
       end
    end,
 }
+
+
+
+
+
+
+
+
+
 
 return build
