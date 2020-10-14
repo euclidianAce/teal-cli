058f503536c31431323e43fd89dbae7b0141f353
Corey Williamson <euclidianAce@protonmail.com>
2020-10-13 20:08:44 -0500

    Add some debug output to the run command

--- build/tlcli/commands/run.lua
+++ build/tlcli/commands/run.lua
@@ -20,6 +20,7 @@ local run = {
    command = function(args)
       local script_args = args["script"]
       local fname = script_args[1]
+      log.debug("Type checking and loading file %s", fname)
       local chunk, err = util.teal.type_check_and_load(fname)
       if not chunk then
          log.error("Error loading %s: %s", ansi.bright.yellow(fname), err)
@@ -59,15 +60,22 @@ local run = {
       table.remove(script_args, 1)
 
       tl.loader()
+      log.debug("Creating thread...")
       local script = coroutine.create(function()
+         log.debug("[thread] Executing...")
+         log.debug("=============================")
          chunk(table.unpack(script_args))
+         log.debug("=============================")
+         log.debug("[thread] done")
       end)
 
       local exit = 0
       repeat
+         log.debug("Resuming thread")
          local ok = coroutine.resume(script)
          if not ok then
             exit = 1
+            log.debug("Error in thread, getting stacktrace and removing traces from this file")
             local trace = debug.traceback(script)
             local stack = util.generate(util.split(trace, "\n", true))
 
@@ -83,8 +91,10 @@ local run = {
 
             log.error("Error in script %s:\nTraceback:\n%s", ansi.bright.yellow(fname), table.concat(stack, "\n"))
 
+            break
          end
       until coroutine.status(script) == "dead"
+      log.debug("Returning with exit code %d", exit)
       return exit
    end,
 }
