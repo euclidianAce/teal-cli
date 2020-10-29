b3b37448d320a7dc7a4289028da1b7156614590a
Corey Williamson <euclidianAce@protonmail.com>
2020-10-28 21:33:25 -0500

    Add bootstrap script for testing

---
+++ bootstrap.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+echo -e "\e[1;32mBuilding with tl...\e[0m" && tl build && \
+	echo -e "\e[1;32mBuilding with tlc via tl compiled code\e[0m" && ./bin/tlc build -u && \
+	rm -r build/tlcli && \
+	echo -e "\e[1;32mReplacing tl compiled code with tlc compiled code\e[0m" && mv tmp/build build/tlcli && \
+	rm -r tmp && \
+	echo -e "\e[1;32mBuilding with tlc via tlc compiled code\e[0m" && ./bin/tlc build -u && \
+	echo -e "\e[1;32mBootstrapping successful\e[0m" || echo -e "\e[1;31mBootstrapping unsuccessful\e[0m"
